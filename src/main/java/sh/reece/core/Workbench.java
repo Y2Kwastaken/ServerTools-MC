d26bd402c3f468a202b6194d51fc458da1d7cd2e
Miles <81843550+Y2Kwastaken@users.noreply.github.com>
2026-09-05 00:25:40 +0000

    update workbench to not use deprecated API (#24)

--- src/main/java/sh/reece/core/Workbench.java
+++ src/main/java/sh/reece/core/Workbench.java
@@ -4,20 +4,27 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+import org.bukkit.inventory.MenuType;
 import sh.reece.tools.BaseCommand;
 import sh.reece.tools.Main;
 
 public class Workbench extends BaseCommand {
 
-	public Workbench(Main instance) {
-		super(instance, "Core.Workbench", "workbench", "craft");
-	}
+    public static final String PLAYER_ONLY = "<red>[!] Only players can open a workbench!";
 
-	@Override
-	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-		if (noPermission(sender, cmd)) return true;
+    public Workbench(Main instance) {
+        super(instance, "Core.Workbench", "workbench", "craft");
+    }
 
-		((Player) sender).openWorkbench(null, true);
-		return true;
-	}
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (noPermission(sender, cmd)) return true;
+
+        if (sender instanceof Player player) {
+            MenuType.CRAFTING.create(player); // unstable in 1.21.5 but stable and unchanged in <=1.21.11
+        } else {
+            sender.sendRichMessage(PLAYER_ONLY);
+        }
+        return true;
+    }
 }
